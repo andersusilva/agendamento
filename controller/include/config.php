064ffcfd561a256b21ffef9a092c588d2e16d62c
Andersu Silva <andersusilva@gmail.com>
2018-06-12 01:44:27 +0000

    Apply fixes from StyleCI

--- controller/include/config.php
+++ controller/include/config.php
@@ -1,12 +1,11 @@
 <?php
-	$host = "localhost";
-	$user = "developf_sala";
-	$pass = "j]8T2fkC?h6h";
-	$base = "developf_agendamento";
-	
-	$conn = new mysqli($host, $user, $pass, $base);
-	
-	if($conn->connect_error){
-		die("Erro: ".$conn->connect_error);
-	}
-?>
+    $host = 'localhost';
+    $user = 'developf_sala';
+    $pass = 'j]8T2fkC?h6h';
+    $base = 'developf_agendamento';
+
+    $conn = new mysqli($host, $user, $pass, $base);
+
+    if ($conn->connect_error) {
+        die('Erro: '.$conn->connect_error);
+    }
--- controller/proc_apagar_evento.php
+++ controller/proc_apagar_evento.php
@@ -1,4 +1,5 @@
 <?php
+
 include_once 'conexao.php';
 $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 if (!empty($id)) {
